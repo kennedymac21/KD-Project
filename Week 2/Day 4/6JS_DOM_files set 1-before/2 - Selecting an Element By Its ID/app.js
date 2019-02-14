e3fe76bc9163aaf5b7cdcf587f2f13cb111ab72a
kennedymac21 <47111181+kennedymac21@users.noreply.github.com>
2019-02-14 12:48:11 +0000

    6JS_DOM_files set 1-before

---
+++ Week 2/Day 4/6JS_DOM_files set 1-before/2 - Selecting an Element By Its ID/app.js
@@ -0,0 +1,7 @@
+const heading = document.getElementById("heading");
+const imput = document.getElementById("imput");
+const button = document.getElementById("button");
+
+
+button.addEventListener("click",()=>{ heading.style.color = input.value;
+});
